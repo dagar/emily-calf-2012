c6b71efe06ca55af61c2d07fedcd9a5e123fe745
Daniel Agar <daniel@agar.ca>
2013-01-29 17:57:05 -0500

    read individual excel sheets

--- analyze.py
+++ analyze.py
@@ -4,24 +4,52 @@ import sys
 import xlrd
 import datetime
 
-def main():
-    print sys.argv[1]
-    workbook = xlrd.open_workbook(sys.argv[1])
+def convert_excel_date(xldate, day):
+    date = xlrd.xldate_as_tuple(xldate, 1)
 
-    worksheets = workbook.sheet_names()
-    #for worksheet_name in worksheets:
+    return datetime.datetime(2012, 06, day, date[3], date[4], date[5])
 
-    worksheet = workbook.sheet_by_name('1A')
-    print worksheet
+
+def read_sheet(xls_name, sheet_name):
+    workbook = xlrd.open_workbook(xls_name)
+    worksheet = workbook.sheet_by_name(sheet_name)
+
+    ret = []
 
     nrows = worksheet.nrows
+    current_meal_num = int(worksheet.cell_value(1, 9))
+    meal_start_time = worksheet.cell_value(1, 4)
     for r in xrange(1, nrows):
-        #print worksheet.row(r)
+        calf = worksheet.cell_value(r, 0)
+        trmt = worksheet.cell_value(r, 1)
+        week = int(worksheet.cell_value(r, 2))
+        day = int(worksheet.cell_value(r, 3))
+        start_time = worksheet.cell_value(r, 4)
+        scan = worksheet.cell_value(r, 5)
+        end_time = worksheet.cell_value(r, 6)
+
         meal_num = int(worksheet.cell_value(r, 9))
-        start_time = xlrd.xldate_as_tuple(worksheet.cell_value(r, 4), 1)
-        end_time = xlrd.xldate_as_tuple(worksheet.cell_value(r, 6), 1)
-        diff_time = xlrd.xldate_as_tuple(worksheet.cell_value(r, 6) - worksheet.cell_value(r, 4), 1)
-        print r, meal_num, start_time, end_time, diff_time
+
+        if r < nrows - 1:
+            next_meal_num = int(worksheet.cell_value(r + 1, 9))
+        else:
+            next_meal_num = -1
+
+        if next_meal_num != meal_num:
+            feeding_time = end_time - meal_start_time
+            ret.append([calf, week, day, convert_excel_date(meal_start_time, day), convert_excel_date(end_time, day), convert_excel_date(feeding_time, day)])
+            current_meal_num = meal_num
+            if next_meal_num == -1:
+                return ret
+            else:
+                meal_start_time = worksheet.cell_value(r + 1, 4)
+
+
+def main():
+    workbook = read_sheet(sys.argv[1], '1A')
+
+    print workbook
+
 
 if __name__ == '__main__':
-  main()
+    main()
